446e8a410a7f6105ff7ebf8331707fdcf69a7696
Felipe <felipe@somacontadigital.com.br>
2022-01-04 20:48:38 -0300

    Create a custom hook to request with axios

---
+++ index.jsx
@@ -0,0 +1,41 @@
+const App = () => {
+  const { response, loading, error } = useAxios({
+      method: 'post',
+      url: '/posts',
+      headers: JSON.stringify({ accept: '*/*' }),
+      body: JSON.stringify({
+        userId: 1,
+        id: 19392,
+        title: 'title',
+        body: 'Sample text',
+      }),
+  });
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
+
+  return (
+    <div className='App'>
+      <h1>Posts</h1>
+
+      {loading ? (
+          <p>loading...</p>
+      ) : (
+        <div>
+            {error && (
+                <div>
+                    <p>{error.message}</p>
+                </div>
+            )}
+            <div>{data && <p>{data.id}</p>}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
